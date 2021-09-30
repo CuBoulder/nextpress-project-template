e2b12ae54ca3415b173bb647092d52460769fc2c
brookestevens <brst4163@colorado.edu>
2021-09-29 19:07:04 -0600

    update actions

---
+++ .github/scripts/generate.php
@@ -0,0 +1,27 @@
+<?php
+
+$composer = null;
+try{
+  $composer = @file_get_contents("https://raw.githubusercontent.com/CuBoulder/nextpress-project-template/main/composer.json");
+  if( $composer === false){
+    throw new Exception("ERROR: Unable to fetch resource \n");
+  }
+}
+catch(Exception $e){
+  echo $e->getMessage();
+}
+// add the packages
+$res =  json_decode($composer, true);
+
+// skip first index since that's the composer package
+for( $i = 1; $i < count( $res['repositories'] ); $i++ ){
+  preg_match( '/CuBoulder\/(.*?)\.git/', $res['repositories'][$i]['url'], $match);
+  $package = $match[1];
+  // add the package into the require section
+  $res['require']['cu-boulder/'.$package] = 'dev-main';
+}
+
+// write the file out
+$composer = json_encode($res, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
+file_put_contents('./composer.json', $composer);
+?>
